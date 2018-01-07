f110639d69971cd2879aae5e17e036ab4b2390db
ZeroZJQGithub <372826058@qq.com>
2018-01-07 08:15:14 -0800

    add Project

---
+++ ios/RCTAliCloudOSS/RCTAliCloudOSS.h
@@ -0,0 +1,20 @@
+//
+//  RCTAliCloudOSS.h
+//  RCTAliCloudOSS
+//
+//  Created by zero on 2018/1/7.
+//  Copyright © 2018年 Canzhijia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
+
+#import "RCTEventEmitter.h"
+
+@interface RCTAliCloudOSS : NSObject
+
+@end
