eebfe8227e623e4a7c1dd1a0956a31a94c993ad5
ZeroZJQGithub <372826058@qq.com>
2018-01-13 21:41:35 -0800

    Update some mistakes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /**
  * @flow
+ * changes 2018-01-14
  */
 'use strict';
 
@@ -17,6 +18,7 @@ const DOWNLOAD_EVENT = 'downloadProgress';
 const _subscriptions = new Map();
 
 const AliCloudOSS = {
+
     //开启oss log
     enableOSSLog() {
         NativeAliCloudOSS.enableOSSLog();
